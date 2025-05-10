22c2a65fcfedb1ef090719b5e7b122440d30c2bd
Djaux <aux130902@gmail.com>
2025-05-10 22:08:09 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+
 app.get("/proxy-stream", async (req, res) => {
   try {
     const stream = await axios.get("http://uk3freenew.listen2myradio.com:7715/stream", {
@@ -5,11 +10,15 @@ app.get("/proxy-stream", async (req, res) => {
     });
 
     res.setHeader("Content-Type", "audio/mpeg");
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "*"); // если нужно CORS
 
     stream.data.pipe(res);
   } catch (e) {
     console.error("Ошибка:", e.message);
     res.status(500).send("Поток не доступен.");
   }
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Proxy is running on port " + (process.env.PORT || 3000));
 });
