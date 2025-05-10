37fb88127b6be7d5f8bdc48accc46384a29607a6
Djaux <aux130902@gmail.com>
2025-05-10 21:56:57 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+
 app.get("/proxy-stream", async (req, res) => {
   try {
     const stream = await axios.get("http://uk3freenew.listen2myradio.com:7715/stream", {
@@ -10,16 +15,24 @@ app.get("/proxy-stream", async (req, res) => {
     });
 
     res.setHeader("Content-Type", "audio/mpeg");
+    res.setHeader("Connection", "keep-alive");
+    res.setHeader("Transfer-Encoding", "chunked");
     res.setHeader("Access-Control-Allow-Origin", "*");
 
-    // Обрыв по уходу клиента
+    // завершение соединения при отключении клиента
     req.on("close", () => {
-      stream.data.destroy();
+      if (stream && stream.data) stream.data.destroy();
     });
 
     stream.data.pipe(res);
   } catch (e) {
-    console.error("Ошибка:", e.message);
-    res.status(500).send("Поток не доступен.");
+    console.error("Ошибка при подключении к Icecast:", e.message);
+    if (!res.headersSent) {
+      res.status(500).send("Ошибка при получении потока.");
+    }
   }
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Proxy is running on port " + (process.env.PORT || 3000));
 });
